05be9ee4ca2332df8f42e2998a74f189f8ddc2f8
MelnikovEI <64632765+MelnikovEI@users.noreply.github.com>
2023-08-01 12:14:40 +0300

    Show cart

--- fish_shop_bot.py
+++ fish_shop_bot.py
@@ -1,6 +1,6 @@
 import datetime
 import logging
-import pprint
+from enum import Enum
 from pathlib import Path
 import redis
 import requests
@@ -13,7 +13,7 @@ from telegram.ext import (
     ConversationHandler,
     CallbackContext,
 )
-# from telegram.utils import types
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -22,9 +22,16 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Stages
-FIRST, SECOND = range(2)
-# Callback data
-START, TWO, THREE, FOUR = range(4)
+CHOOSE, FILL_CART, HANDLE_CART = range(3)
+
+
+class Buttons(Enum):
+    START = 0
+    ONE = 1
+    FIVE = 5
+    TEN = 10
+    CART = -1
+
 
 env = Env()
 env.read_env()
@@ -54,7 +61,6 @@ def get_cms_token():
         token_data = response.json()
         redis_db.set('expires', token_data.get('expires'))
         redis_db.set('access_token', token_data.get('access_token'))
-        # print(token_data.get('expires'), token_data.get('access_token'))
         return token_data.get('access_token')
 
 
@@ -67,7 +73,6 @@ def get_all_products():
     response = requests.get(url, headers=header)
     response.raise_for_status()
     products_data = response.json()['data']
-    # pprint.pprint(products_data)
     products = []
     for product in products_data:
         products.append(
@@ -96,7 +101,6 @@ def get_product_info(product_id):
         get_product_price(product_id),
         products_data.get('attributes').get('description', ''),
     ]
-    # pprint.pprint(products_data)
     return '\n'.join(product_info)
 
 
@@ -133,6 +137,61 @@ def get_product_image(product_id):
     return file_path
 
 
+def add_to_cart(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    query.answer()
+    quantity, product_id = query.data.split(maxsplit=1)
+
+    header = {
+        'Authorization': f'Bearer {get_cms_token()}',
+        }
+    url_path = f'/v2/carts/{update.effective_user.id}/items'
+    url = f'{BASE_URL}{url_path}'
+    data = {
+        'data': {
+            'id': product_id,
+            'type': 'cart_item',
+            'quantity': int(quantity),
+        }
+    }
+    response = requests.post(url, headers=header, json=data)
+    response.raise_for_status()
+    return FILL_CART
+
+
+def show_cart(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    query.answer()
+    header = {
+        'Authorization': f'Bearer {get_cms_token()}',
+        }
+    url_path = f'/v2/carts/{update.effective_user.id}/items'
+    url = f'{BASE_URL}{url_path}'
+    response = requests.get(url, headers=header)
+    response.raise_for_status()
+    cart = response.json()
+    products = []
+    for product in cart['data']:
+        products.extend((
+            product['name'],
+            f"{product['meta']['display_price']['with_tax']['unit']['formatted']} per kg",
+            f"{product['quantity']} kg in cart for ${product['value']['amount'] / 100}",
+            ''
+        ))
+    products.append(f"Total: {cart['meta']['display_price']['with_tax']['formatted']}")
+    cart_info = 'Your cart:\n\n' + '\n'.join(products)
+    keyboard = [
+        [InlineKeyboardButton("-", callback_data=f'{str(Buttons.ONE.value)}'),],
+        [InlineKeyboardButton("Back to main menu", callback_data=Buttons.START.value),]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    logo = Path.cwd() / 'images' / 'logo.png'
+    with open(logo, 'rb') as photo:
+        query.edit_message_media(media=InputMediaPhoto(media=photo, caption=cart_info), reply_markup=reply_markup)
+
+    return HANDLE_CART
+
+
 def start(update: Update, context: CallbackContext) -> int:
     """Send message on `/start`."""
     # Get user that sent /start and log his name
@@ -148,6 +207,7 @@ def start(update: Update, context: CallbackContext) -> int:
                 InlineKeyboardButton(product['name'], callback_data=product['id'])
             ]
         )
+    buttons.append([InlineKeyboardButton('Cart', callback_data=str(Buttons.CART.value))])
     keyboard = buttons
     reply_markup = InlineKeyboardMarkup(keyboard)
     # Send message with text and appended InlineKeyboard
@@ -155,7 +215,7 @@ def start(update: Update, context: CallbackContext) -> int:
     logo = Path.cwd() / 'images' / 'logo.png'
     update.message.reply_photo(photo=open(logo, 'rb'), caption='Please, choose:', reply_markup=reply_markup)
     # Tell ConversationHandler that we're in state `FIRST` now
-    return FIRST
+    return CHOOSE
 
 
 def start_over(update: Update, context: CallbackContext) -> int:
@@ -173,6 +233,7 @@ def start_over(update: Update, context: CallbackContext) -> int:
                 InlineKeyboardButton(product['name'], callback_data=product['id'])
             ]
         )
+    buttons.append([InlineKeyboardButton('Cart', callback_data=str(Buttons.CART.value))])
     keyboard = buttons
     reply_markup = InlineKeyboardMarkup(keyboard)
     # Instead of sending a new message, edit the message that originated the CallbackQuery. This gives the feeling of an
@@ -182,27 +243,30 @@ def start_over(update: Update, context: CallbackContext) -> int:
     with open(logo, 'rb') as photo:
         query.edit_message_media(media=InputMediaPhoto(media=photo, caption='Please, choose:'), reply_markup=reply_markup)
 
-    return FIRST
+    return CHOOSE
 
 
 def show_product_info(update: Update, context: CallbackContext) -> int:
     """Show new choice of buttons"""
     query = update.callback_query
     query.answer()
+    product_id = query.data
     keyboard = [
         [
-            InlineKeyboardButton("Back to main menu", callback_data=str(START)),
-            # InlineKeyboardButton("4", callback_data=str(FOUR)),
-        ]
+            InlineKeyboardButton("1 kg", callback_data=f'{str(Buttons.ONE.value)} {product_id}'),
+            InlineKeyboardButton("5 kg", callback_data=f'{str(Buttons.FIVE.value)} {product_id}'),
+            InlineKeyboardButton("10 kg", callback_data=f'{str(Buttons.TEN.value)} {product_id}'),
+        ],
+        [InlineKeyboardButton('Cart', callback_data=str(Buttons.CART.value))],
+        [InlineKeyboardButton("Back to main menu", callback_data=Buttons.START.value)]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    product_id = query.data
+
     product_info = get_product_info(product_id)
-    # query.edit_message_text(text=product_info, reply_markup=reply_markup)
     image = get_product_image(product_id)
     with open(image, 'rb') as photo:
         query.edit_message_media(media=InputMediaPhoto(media=photo, caption=product_info), reply_markup=reply_markup)
-    return FIRST
+    return FILL_CART
 
 
 def end(update: Update, context: CallbackContext) -> int:
@@ -226,16 +290,20 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            FIRST: [
-                CallbackQueryHandler(start_over, pattern='^' + str(START) + '$'),
+            CHOOSE: [
+                CallbackQueryHandler(start_over, pattern='^' + str(Buttons.START.value) + '$'),
+                CallbackQueryHandler(show_cart, pattern='^' + str(Buttons.CART.value) + '$'),
                 CallbackQueryHandler(show_product_info),
-                # CallbackQueryHandler(two, pattern='^' + str(TWO) + '$'),
-                # CallbackQueryHandler(three, pattern='^' + str(THREE) + '$'),
-                # CallbackQueryHandler(four, pattern='^' + str(FOUR) + '$'),
             ],
-            SECOND: [
-                # CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
-                CallbackQueryHandler(end, pattern='^' + str(TWO) + '$'),
+            FILL_CART: [
+                CallbackQueryHandler(start_over, pattern='^' + str(Buttons.START.value) + '$'),
+                CallbackQueryHandler(show_cart, pattern='^' + str(Buttons.CART.value) + '$'),
+                CallbackQueryHandler(add_to_cart),
+                # CallbackQueryHandler(end, pattern='^' + str('') + '$'),
+            ],
+            HANDLE_CART: [
+                CallbackQueryHandler(start_over, pattern='^' + str(Buttons.START.value) + '$'),
+                # CallbackQueryHandler(end, pattern='^' + str('') + '$'),
             ],
         },
         fallbacks=[CommandHandler('start', start)],

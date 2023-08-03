bec6b8583c02e33779938174d3985a9925e708b4
MelnikovEI <64632765+MelnikovEI@users.noreply.github.com>
2023-08-03 16:40:14 +0300

    Get a product price from CMS

--- fish_shop_bot.py
+++ fish_shop_bot.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import pprint
 from enum import Enum
 from pathlib import Path
 import redis
@@ -11,31 +12,31 @@ from telegram.ext import (
     CommandHandler,
     CallbackQueryHandler,
     ConversationHandler,
-    CallbackContext,
+    CallbackContext, MessageHandler, Filters,
 )
 
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
-
 logger = logging.getLogger(__name__)
-
-# Stages
-CHOOSE, FILL_CART, HANDLE_CART = range(3)
-
-
-class Buttons(Enum):
-    START = 0
-    ONE = 1
-    FIVE = 5
-    TEN = 10
-    CART = -1
-
 
 env = Env()
 env.read_env()
 BASE_URL = env('BASE_URL')
+
+CHOOSE, FILL_CART, HANDLE_CART, WAITING_EMAIL, END = range(5)  # Statuses
+
+
+class Buttons(Enum):
+    START = 0
+    CART = -1
+    PAY = -2
+    LEAVE = -3
+    ONE = 1
+    FIVE = 5
+    TEN = 10
+
 
 redis_db = redis.Redis(
     host=env('DATABASE_HOST'), port=env('DATABASE_PORT'), username=env('DATABASE_USERNAME'),
@@ -84,12 +85,25 @@ def get_all_products():
     return products
 
 
+def get_product_price(product_id):
+    header = {
+        'Authorization': f'Bearer {get_cms_token()}',
+    }
+    url_path = f'/catalog/products/{product_id}'
+    url = f'{BASE_URL}{url_path}'
+    response = requests.get(url, headers=header)
+    response.raise_for_status()
+    price_data = response.json()['data']
+    product_price = int(price_data['attributes']['price']['USD']['amount'])/100
+    return f'${product_price} per kg'
+
+
 def get_product_details(product_id):
     header = {
         'Authorization': f'Bearer {get_cms_token()}',
         }
     params = {
-        'filter': f'in(id,{product_id})'
+        'filter': f'eq(id,{product_id})'
     }
     url_path = '/pcm/products'
     url = f'{BASE_URL}{url_path}'
@@ -102,11 +116,6 @@ def get_product_details(product_id):
         products_data.get('attributes').get('description', ''),
     ]
     return '\n'.join(product_details)
-
-
-def get_product_price(product_id):
-    price_book_id = env('PRICE_BOOK_ID')
-    return '$1 per kg'
 
 
 def get_product_image(product_id):
@@ -133,7 +142,6 @@ def get_product_image(product_id):
         Path(Path.cwd() / 'images').mkdir(parents=True, exist_ok=True)
         with open(file_path, 'wb') as file_to_save:
             file_to_save.write(response.content)
-        print('Скачал')
     return file_path
 
 
@@ -155,6 +163,7 @@ def add_to_cart(update: Update, context: CallbackContext) -> int:
     }
     response = requests.post(url, headers=header, json=data)
     response.raise_for_status()
+    redis_db.hset(update.effective_user.id, mapping={'status': FILL_CART})
     return FILL_CART
 
 
@@ -201,71 +210,62 @@ def show_cart(update: Update, context: CallbackContext) -> int:
     products.append(f"Total: {cart['meta']['display_price']['with_tax']['formatted']}")
     cart_content = 'Your cart:\n\n' + '\n'.join(products)
 
+    keyboard.append([InlineKeyboardButton("Pay", callback_data=Buttons.PAY.value)])
     keyboard.append([InlineKeyboardButton("Back to main menu", callback_data=Buttons.START.value)])
+
     reply_markup = InlineKeyboardMarkup(keyboard)
     logo = Path.cwd() / 'images' / 'logo.png'
     with open(logo, 'rb') as photo:
         query.edit_message_media(media=InputMediaPhoto(media=photo, caption=cart_content), reply_markup=reply_markup)
 
+    redis_db.hset(update.effective_user.id, mapping={'status': HANDLE_CART})
     return HANDLE_CART
 
 
 def start(update: Update, context: CallbackContext) -> int:
-    """Send message on `/start`."""
-    # Get user that sent /start and log his name
     user = update.message.from_user
     logger.info("User %s started the conversation.", user.first_name)
-    # Build InlineKeyboard where each button has a displayed text a string as callback_data
-    # The keyboard is a list of button rows, where each row is in turn a list (hence `[[...]]`).
     products = get_all_products()
-    buttons = []
+    keyboard = []
     for product in products:
-        buttons.append(
-            [
-                InlineKeyboardButton(product['name'], callback_data=product['id'])
-            ]
-        )
-    buttons.append([InlineKeyboardButton('Cart', callback_data=str(Buttons.CART.value))])
-    keyboard = buttons
+        keyboard.append([InlineKeyboardButton(product['name'], callback_data=product['id'])])
+    keyboard.append([InlineKeyboardButton('Cart', callback_data=str(Buttons.CART.value))])
+    keyboard.append([InlineKeyboardButton('Leave', callback_data=str(Buttons.LEAVE.value))])
     reply_markup = InlineKeyboardMarkup(keyboard)
-    # Send message with text and appended InlineKeyboard
-    # update.message.reply_text('Please, choose:', reply_markup=reply_markup)
+
     logo = Path.cwd() / 'images' / 'logo.png'
-    update.message.reply_photo(photo=open(logo, 'rb'), caption='Please, choose:', reply_markup=reply_markup)
-    # Tell ConversationHandler that we're in state `FIRST` now
+    with open(logo, 'rb') as photo:
+        update.message.reply_photo(photo=photo, caption='Please, choose:', reply_markup=reply_markup)
+    redis_db.hset(update.effective_user.id, mapping={'status': CHOOSE})
     return CHOOSE
 
 
 def start_over(update: Update, context: CallbackContext) -> int:
-    """Prompt same text & keyboard as `start` does but not as new message"""
-    # Get CallbackQuery from Update
     query = update.callback_query
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     query.answer()
     products = get_all_products()
-    buttons = []
+    keyboard = []
     for product in products:
-        buttons.append(
+        keyboard.append(
             [
                 InlineKeyboardButton(product['name'], callback_data=product['id'])
             ]
         )
-    buttons.append([InlineKeyboardButton('Cart', callback_data=str(Buttons.CART.value))])
-    keyboard = buttons
+    keyboard.append([InlineKeyboardButton('Cart', callback_data=str(Buttons.CART.value))])
+    keyboard.append([InlineKeyboardButton('Leave', callback_data=str(Buttons.LEAVE.value))])
     reply_markup = InlineKeyboardMarkup(keyboard)
-    # Instead of sending a new message, edit the message that originated the CallbackQuery. This gives the feeling of an
-    # interactive menu.
-    # query.edit_message_text(text='Please, choose:', reply_markup=reply_markup)
+
     logo = Path.cwd() / 'images' / 'logo.png'
     with open(logo, 'rb') as photo:
-        query.edit_message_media(media=InputMediaPhoto(media=photo, caption='Please, choose:'), reply_markup=reply_markup)
-
+        query.edit_message_media(
+            media=InputMediaPhoto(media=photo, caption='Please, choose:'),
+            reply_markup=reply_markup
+        )
+    redis_db.hset(update.effective_user.id, mapping={'status': CHOOSE})
     return CHOOSE
 
 
 def show_product(update: Update, context: CallbackContext) -> int:
-    """Show new choice of buttons"""
     query = update.callback_query
     query.answer()
     product_id = query.data
@@ -284,59 +284,109 @@ def show_product(update: Update, context: CallbackContext) -> int:
     image = get_product_image(product_id)
     with open(image, 'rb') as photo:
         query.edit_message_media(media=InputMediaPhoto(media=photo, caption=product_details), reply_markup=reply_markup)
+
+    redis_db.hset(update.effective_user.id, mapping={'status': FILL_CART})
     return FILL_CART
 
 
-def end(update: Update, context: CallbackContext) -> int:
-    """Returns `ConversationHandler.END`, which tells the
-    ConversationHandler that the conversation is over.
-    """
+def pay(update: Update, context: CallbackContext) -> int:
     query = update.callback_query
     query.answer()
-    query.edit_message_text(text="See you next time!")
+
+    logo = Path.cwd() / 'images' / 'logo.png'
+    with open(logo, 'rb') as photo:
+        query.edit_message_media(media=InputMediaPhoto(media=photo, caption='Enter your e-mail, please'))
+
+    redis_db.hset(update.effective_user.id, mapping={'status': WAITING_EMAIL})
+    return WAITING_EMAIL
+
+
+def incorrect_email(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text("We've got incorrect e-mail from you :(. Please try again.")
+
+    redis_db.hset(update.effective_user.id, mapping={'status': WAITING_EMAIL})
+    return WAITING_EMAIL
+
+
+def add_customer(update: Update, context: CallbackContext) -> int:
+    email = update.message.text
+    header = {
+        'Authorization': f'Bearer {get_cms_token()}',
+    }
+    params = {
+        'filter': f'eq(email,{email})'
+    }
+    url_path = f'/v2/customers'
+    url = f'{BASE_URL}{url_path}'
+    response = requests.get(url, headers=header, params=params)
+    response.raise_for_status()
+    customer_from_cms = response.json()
+
+    if not customer_from_cms['data']:
+        header = {
+            'Authorization': f'Bearer {get_cms_token()}',
+            }
+        data = {
+            'data': {
+                'type': 'customer',
+                'name': update.effective_user.name,
+                'email': email,
+            }
+        }
+        url_path = f'/v2/customers'
+        url = f'{BASE_URL}{url_path}'
+        response = requests.post(url, headers=header, json=data)
+        response.raise_for_status()
+
+    return end(update, context)
+
+
+def end(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    if query:
+        query.answer()
+        logo = Path.cwd() / 'images' / 'logo.png'
+        with open(logo, 'rb') as photo:
+            query.edit_message_media(media=InputMediaPhoto(media=photo, caption='Thank you! See you next time!'))
+    else:
+        update.message.reply_text('Thank you! See you next time!')
+    redis_db.hset(update.effective_user.id, mapping={'status': END})
     return ConversationHandler.END
 
 
 def main() -> None:
     tg_bot_token = env('TG_BOT_TOKEN')
     updater = Updater(tg_bot_token)
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
-    # Setup conversation handler with the states FIRST and SECOND
-    # Use the pattern parameter to pass CallbackQueries with specific data pattern to the corresponding handlers.
-    # ^ means "start of line/string" $ means "end of line/string" So ^ABC$ will only allow 'ABC'
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
             CHOOSE: [
                 CallbackQueryHandler(start_over, pattern='^' + str(Buttons.START.value) + '$'),
                 CallbackQueryHandler(show_cart, pattern='^' + str(Buttons.CART.value) + '$'),
+                CallbackQueryHandler(end, pattern='^' + str(Buttons.LEAVE.value) + '$'),
                 CallbackQueryHandler(show_product),
             ],
             FILL_CART: [
                 CallbackQueryHandler(start_over, pattern='^' + str(Buttons.START.value) + '$'),
                 CallbackQueryHandler(show_cart, pattern='^' + str(Buttons.CART.value) + '$'),
                 CallbackQueryHandler(add_to_cart),
-                # CallbackQueryHandler(end, pattern='^' + str('') + '$'),
             ],
             HANDLE_CART: [
                 CallbackQueryHandler(start_over, pattern='^' + str(Buttons.START.value) + '$'),
+                CallbackQueryHandler(pay, pattern='^' + str(Buttons.PAY.value) + '$'),
                 CallbackQueryHandler(delete_from_cart),
-                # CallbackQueryHandler(end, pattern='^' + str('') + '$'),
+            ],
+            WAITING_EMAIL: [
+                MessageHandler(Filters.entity('email'), add_customer),
+                MessageHandler(Filters.all, incorrect_email)
             ],
         },
         fallbacks=[CommandHandler('start', start)],
     )
 
-    # Add ConversationHandler to dispatcher that will be used for handling updates
     dispatcher.add_handler(conv_handler)
-
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
